7ab343a3fc59d253b2a9eafc4fb60b19b8c186c2
Luciano <lucciano01@hotmail.com>
2019-08-20 21:51:19 -0300

    Mapeando relacionamentos muitos para muitos

--- src/main/java/com/luciano/cursomc/CursoMcApplication.java
+++ src/main/java/com/luciano/cursomc/CursoMcApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.luciano.cursomc.domain.Categoria;
+import com.luciano.cursomc.domain.Produto;
 import com.luciano.cursomc.repositories.CategoriaRepository;
+import com.luciano.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoMcApplication implements CommandLineRunner {
@@ -16,7 +18,10 @@ public class CursoMcApplication implements CommandLineRunner {
 	//a interface CommandLineRunner permite executar alguma ação antes da aplicação iniciar
 	
 	@Autowired
-	private CategoriaRepository repo;
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
@@ -26,7 +31,20 @@ public class CursoMcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria c1 = new Categoria(null, "Informática");
 		Categoria c2 = new Categoria(null, "Escritório");
-		repo.saveAll(Arrays.asList(c1,c2));
+		Produto p1 = new Produto(null, "Computador", 2000.0);
+		Produto p2 = new Produto(null, "Impressora", 800.0);
+		Produto p3 = new Produto(null, "Mouse", 80.0);
+		
+		c1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		c2.getProdutos().addAll(Arrays.asList(p2 ));
+		
+		p1.getCategorias().addAll(Arrays.asList(c1));
+		p2.getCategorias().addAll(Arrays.asList(c1,c2));
+		p3.getCategorias().addAll(Arrays.asList(c1));
+		
+		categoriaRepository.saveAll(Arrays.asList(c1,c2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
 	}
 
 }
