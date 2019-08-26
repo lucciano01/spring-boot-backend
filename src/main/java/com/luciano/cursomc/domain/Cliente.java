74d545ea192ea610e8c47b15095f3ac747519014
Luciano <lucciano01@hotmail.com>
2019-08-26 17:13:42 -0300

    alterando as anotacoes JsonBackReference para JsonIgnore

--- src/main/java/com/luciano/cursomc/domain/Cliente.java
+++ src/main/java/com/luciano/cursomc/domain/Cliente.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.luciano.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -32,7 +31,6 @@ public class Cliente implements Serializable {
 	private Integer tipoCliente;
 	
 	@OneToMany(mappedBy="cliente")
-	@JsonManagedReference
 	private List<Endereco> enderecos = new ArrayList<Endereco>();
 
 	@ElementCollection
@@ -40,7 +38,7 @@ public class Cliente implements Serializable {
 	private Set<String> telefones = new HashSet<>();
 	
 	@OneToMany(mappedBy="cliente")
-	@JsonBackReference
+	@JsonIgnore
 	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
