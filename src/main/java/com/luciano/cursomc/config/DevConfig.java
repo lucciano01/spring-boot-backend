9052f62d1b3de6a642a3c8c333844208301d1fbd
Luciano <lucciano01@hotmail.com>
2019-10-19 15:59:17 -0300

    cofigurando conexao banco de dados

---
+++ src/main/java/com/luciano/cursomc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.luciano.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.luciano.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	//pega o valor dessa propriedade do hibernate do arquivo properties
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDataBase() throws ParseException {
+	 if(!strategy.equals("create")) {
+		return false;
+	 }
+		 dbService.instantiateTestDataBase();
+		return true;
+	}
+
+}
